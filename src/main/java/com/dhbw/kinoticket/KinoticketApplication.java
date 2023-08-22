1824cdc8c433ee2824a6f3de3ad8b008a6c5270f
Konrad Lorenz <konrad-lorenz@outlook.com>
2023-08-22 19:54:07 +0200

    Implementing of Doc for API basic Functionality

--- src/main/java/com/dhbw/kinoticket/KinoticketApplication.java
+++ src/main/java/com/dhbw/kinoticket/KinoticketApplication.java
@@ -1,9 +1,11 @@
 package com.dhbw.kinoticket;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class KinoticketApplication {
 
 	public static void main(String[] args) {

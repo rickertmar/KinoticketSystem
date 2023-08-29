f419a39b91b7837f3e97c4e9d29925a9ab56ea53
Marian Rickert <marian.rickert@gmail.com>
2023-08-29 15:22:09 +0200

    fic 3

---
+++ src/main/java/com/dhbw/kinoticket/entity/LocationAddress.java
@@ -0,0 +1,20 @@
+package com.dhbw.kinoticket.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "LocationAddress")
+public class LocationAddress {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String street;
+    private String city;
+    private String country;
+    private String postcode;
+
+}
